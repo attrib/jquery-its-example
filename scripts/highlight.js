9dd1aa30c613be6cfb70684c21e7024eb4072c3b
kfritsche <karl.fritsche@cocomore.com>
2013-02-22 16:48:00 +0100

    Initial commit

---
+++ scripts/highlight.js
@@ -0,0 +1,39 @@
+function addClass(element, type, checked) {
+  var className = 'its-'+type;
+  if (checked)
+    $(element).addClass(className);
+  else
+    $(element).removeClass(className);
+}
+
+function addClassAndTip(element, type, checked) {
+  addClass(element, type, checked);
+  var $element = $(element);
+  var itsData = $element.getITSData();
+  var toolTip = type+'<br>';
+  $.each(itsData, function(key, value) {
+    toolTip += key + '=' + value + '<br>';
+  });
+  $element.simpletip({content: toolTip});
+}
+
+$(function() {
+  $.parseITS(function() {
+    $('#its-highlighter').find('input[type=checkbox]').click(function() {
+      var $this = $(this);
+      var data = $this.data();
+      var id = this.id;
+      if (data.selector && data.callbackName)
+        $(data.selector).each(function() {
+          if(typeof window[data.callbackName] == 'function') window[data.callbackName](this, id, $this.is(':checked'));
+        });
+    });
+
+    $(':translate(no)').each(function() {
+      $(this).simpletip({
+        content: 'This part will not be translated'
+      });
+    });
+  });
+
+});
