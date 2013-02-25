309a74d4bb19c6aa54095de5941f0da7fbb390fb
kfritsche <karl.fritsche@cocomore.com>
2013-02-25 20:08:01 +0100

    Added collapsed selection

--- scripts/edit.js
+++ scripts/edit.js
@@ -31,8 +31,36 @@ $(function() {
     if (window.getSelection) {
       var selection = window.getSelection();
 
-      console.log(selection.rangeCount, selection.anchorNode, selection.anchorOffset, selection.focusNode, selection.focusOffset);
+      if (selection.rangeCount) {
+        var range = selection.getRangeAt(0);
+        if (range.collapsed) {
+          var currentNode = range.startContainer;
+          while (currentNode.nodeName === '#text' && currentNode.parentNode) {
+            currentNode = currentNode.parentNode;
+          }
+          if (currentNode.nodeName === '#text') {
+            // TODO use the correct attr here! (and be aware of unselect)
+            var $span = $(document.createElement('span')).attr('translate', 'no');
+            $span.html(currentNode);
+            currentNode = $span.get(0);
+            range.insertNode(currentNode);
+          }
+          else {
+            // TODO use the correct attr here! (and be aware of unselect)
+            $(currentNode).attr('translate', 'no');
+          }
+          // Refresh the highlight of this element, if the checkbox is checked
+          if ($('#' + type).is(':checked')) {
+            addClassAndTip(currentNode, type, true);
+          }
+        }
+        else {
+          // TODO selection
+        }
+      }
     }
 
   });
 });
+
+// $($('*[title]').get(0).attributes[1]).is(':translate(yes)')
--- scripts/highlight.js
+++ scripts/highlight.js
@@ -4,38 +4,39 @@
  * Depends on jQuery, ITS-parser plugin and simpletip.
  */
 
-$(function() {
-  /**
-   * Add Class and a JS Tooltip to the element with the type
-   * @param element HTMLNode a node which has this data category
-   * @param type string with the type of the data category
-   * @param checked boolean true if the checkbox is now selected
-   */
-  function addClassAndTip(element, type, checked) {
-    var $element = $(element);
-    var itsData = $element.getITSData();
-    var className = 'its-'+type;
-    if (itsData.translate && type !== 'translate')
-      delete itsData.translate;
-    if (checked) {
-      var toolTip = '';
-      if (type === 'translate') {
-        toolTip = 'This part will not be translated';
-      }
-      else {
-        $.each(itsData, function(key, value) {
-          toolTip += key + '=' + value + '<br>';
-        });
-      }
-      $element.simpletip({content: toolTip, fixed: false});
-      $element.addClass(className);
+/**
+ * Add Class and a JS Tooltip to the element with the type
+ * @param element HTMLNode a node which has this data category
+ * @param type string with the type of the data category
+ * @param checked boolean true if the checkbox is now selected
+ */
+function addClassAndTip(element, type, checked) {
+  var $element = $(element);
+  var itsData = $element.getITSData();
+  var className = 'its-'+type;
+  if (itsData.translate && type !== 'translate')
+    delete itsData.translate;
+  if (checked) {
+    var toolTip = '';
+    if (type === 'translate') {
+      toolTip = 'This part will not be translated';
     }
     else {
-      $element.removeClass(className);
-      $('> .tooltip', $element).remove();
-      $element.removeData('simpletip');
+      $.each(itsData, function(key, value) {
+        toolTip += key + '=' + value + '<br>';
+      });
     }
+    $element.simpletip({content: toolTip, fixed: false});
+    $element.addClass(className);
   }
+  else {
+    $element.removeClass(className);
+    $('> .tooltip', $element).remove();
+    $element.removeData('simpletip');
+  }
+}
+
+$(function() {
 
   /**
    * Parse all ITS data, including global rules.
